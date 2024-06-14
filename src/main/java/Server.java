424fac050f1e15ca2580dfc29215e8db8534cb84
elevne <cwi5525@naver.com>
2024-06-14 12:19:40 +0900

    Server 수정

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,23 +8,18 @@ import java.util.concurrent.Executors;
 public class Server {
     private static final int SERVER_PORT = 40000;
     private static final int THREAD_POOL_SIZE = 10;
-    public static final List<String> INFO_LIST = new Vector<>();
+    public static List<String> INFO_LIST = new Vector<>();
 
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
             try (Socket myClient = serverSocket.accept();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(myClient.getInputStream()));
                 PrintWriter writer = new PrintWriter(myClient.getOutputStream())) {
+                INFO_LIST = new Vector<>();
                 System.out.println("Server is running...");
                 while (true) {
                     Socket clientSocket = serverSocket.accept();
-                    executor.execute(new TokenHandler(clientSocket));
-                    clientSocket.close();
-                    if (INFO_LIST.size() == 5) {
-                        writer.println(INFO_LIST.toString());
-                        break;
-                    }
+                    new TokenHandler(clientSocket, writer).start();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -36,17 +31,24 @@ public class Server {
     }
 }
 
-class TokenHandler implements Runnable {
+class TokenHandler extends Thread {
     private Socket clientSocket;
-    public TokenHandler(Socket clientSocket) {
+    private PrintWriter writer;
+    public TokenHandler(Socket clientSocket, PrintWriter writer) {
         this.clientSocket = clientSocket;
+        this.writer = writer;
     }
 
     @Override
     public void run() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
             String token = reader.readLine();
+            System.out.println(token);
             Server.INFO_LIST.add(token);
+            clientSocket.close();
+            if (Server.INFO_LIST.size() == 5) {
+                writer.println(Server.INFO_LIST.toString());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
